212222ce4de43bc1d0239008ba1f2096ff621278
kishore7cm <chandrke@mail.uc.edu>
2019-11-20 14:08:58 -0500

    Display Crime data

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using NeighborhoodLocation;
+using NeighborhoodCrime;
 
 namespace Neighbourhood_Alert.Pages
 {
@@ -19,7 +22,21 @@ namespace Neighbourhood_Alert.Pages
 
         public void OnGet()
         {
+            IList<Neighborhood> NeighborhoodList = new List<Neighborhood>();
+            IList<Crime> NeighborhoodCrimeList = new List<Crime>();
 
+            using (var webClient = new WebClient())
+            {
+                // string neighborhood = GetData("https://cagisonline.hamilton-co.org/arcgis/rest/services/CINC_PLANNING/Munitwps_Neighborhoods/MapServer/1/query?where=1%3D1&outFields=OBJECTID,CAGIS.Cinc_Community_Council_Bnd.AREA,PERIMETER,NEIGH_BND_,NEIGH,LINE,ACRES,NEIGH_BOUN,REVISED_DA&returnGeometry=false&outSR=4326&f=json");
+                string neighbourhoodjsonString = "";
+                neighbourhoodjsonString = webClient.DownloadString("https://cagisonline.hamilton-co.org/arcgis/rest/services/CINC_PLANNING/Munitwps_Neighborhoods/MapServer/1/query?where=1%3D1&outFields=OBJECTID,CAGIS.Cinc_Community_Council_Bnd.AREA,PERIMETER,NEIGH_BND_,NEIGH,LINE,ACRES,NEIGH_BOUN,REVISED_DA&returnGeometry=false&outSR=4326&f=json");
+                var neighbourhoodlocations = Neighborhood.FromJson(neighbourhoodjsonString);
+                ViewData["neighbourhood"] = neighbourhoodlocations;
+
+                string crimejsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/k59e-2pvf.json");
+                var crimes = Crime.FromJson(crimejsonString);
+                ViewData["crimes"] = crimes;
+            }
         }
     }
 }
